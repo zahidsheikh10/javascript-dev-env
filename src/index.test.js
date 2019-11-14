14f475da9fa1274c9f6a87be588e8190a1a120b7
Sheikh <ZahidHussain.Sheikh@thomsonreuters.com>
2019-11-14 09:34:19 +0530

    updating test.js

--- src/index.test.js
+++ src/index.test.js
@@ -1,22 +1,21 @@
-import {expect} from 'chai';
+import { expect } from 'chai';
 import jsdom from 'jsdom';
 import fs from 'fs';
 
 describe('Our first test', () => {
-  it('should pass',() => {
+  it('should pass', () => {
     expect(true).to.equal(true);
   });
 });
 
-describe('index.html',() => {
-  it('should say hello',(done) => {
-    const index = fs.readFileSync('./src/index.html',"utf-8");
-    jsdom.env(index,function(err,window){
-      const h1 = window.document.getElementByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal('HelloWorld!');
+describe('index.html', () => {
+  it('should have h1 that says Users', (done) => {
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+    jsdom.env(index, function (err, window) {
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal("Hello World!..");
       done();
       window.close();
-    })
+    });
   })
 })
-
