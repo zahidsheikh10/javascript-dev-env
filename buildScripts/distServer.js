eab298059db5c9afdb539ae557c00e20a874d5f5
Sheikh <ZahidHussain.Sheikh@thomsonreuters.com>
2019-11-19 09:49:13 +0530

    update base url to use heroku

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -9,16 +9,8 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(compression());
-app.use(express.static('dist'));
 
-app.get('/users', (req, res) => {
-    // Hard coding for simplicity. Pretend this hits a real database
-    res.json([
-        { "id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com" },
-        { "id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "tnorton@yahoo.com" },
-        { "id": 3, "firstName": "Tina", "lastName": "Lee", "email": "lee.tina@hotmail.com" }
-    ]);
-});
+app.use(express.static('dist'));
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, '../dist/index.html'));
